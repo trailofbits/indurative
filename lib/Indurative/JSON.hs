c6af9dcb7649ded59c8054446a7e303cede289b0
JP Smith <jp@writes.co.de>
2019-03-26 18:58:03 -0400

    add (kinda sketchy) authed json

---
+++ lib/Indurative/JSON.hs
@@ -0,0 +1,87 @@
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE PatternSynonyms #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE ViewPatterns #-}
+
+module Indurative.JSON where
+
+import Control.Lens
+import Control.Monad (join)
+import Crypto.Hash (Digest, HashAlgorithm, SHA3_256)
+import Data.Aeson
+import Data.Aeson.Lens
+import Data.List (sortBy)
+import Data.Maybe (isJust)
+import Data.Ord (comparing)
+import Data.Text (Text)
+import Indurative
+
+data JSONPath a = Here | AtKey Text a (JSONPath a) | AtIx Int a (JSONPath a) deriving (Eq, Functor, Ord, Show)
+
+tOf :: JSONPath a -> Traversal' Value Value
+tOf Here          = ignored
+tOf (AtKey k _ _) = key k
+tOf (AtIx  i _ _) = nth i
+
+nextPath :: JSONPath a -> JSONPath a
+nextPath Here          = Here
+nextPath (AtKey _ _ m) = m
+nextPath (AtIx  _ _ m) = m
+
+withStartOf :: JSONPath a -> b -> JSONPath b -> JSONPath b
+withStartOf Here          _ _ = Here
+withStartOf (AtKey k _ _) b p = AtKey k b p
+withStartOf (AtIx  k _ _) b p = AtIx  k b p
+
+jhash :: HashAlgorithm a => Value -> Digest a
+jhash = hash . encode
+
+jTopHash :: HashAlgorithm a => Value -> Digest a
+jTopHash v@(Object (null -> empty)) = if empty then hash ("empty object" :: Text) else
+  foldr1 hashCons $ jTopHash . snd <$> sortBy (comparing fst) (v ^@.. members)
+jTopHash v@(Array (null -> empty)) = if empty then hash ("empty array" :: Text) else
+  foldr1 hashCons $ jTopHash . snd <$> sortBy (comparing fst) (v ^@.. values)
+jTopHash v = jhash v
+
+jFoldPath :: (a -> a -> a) -> a -> JSONPath ([a], [a]) -> a
+jFoldPath _ i Here               = i
+jFoldPath f i (AtKey _ (l, r) n) = foldr1 f (l ++ [jFoldPath f i n] ++ r)
+jFoldPath f i (AtIx  _ (l, r) n) = foldr1 f (l ++ [jFoldPath f i n] ++ r)
+
+data IxedJSON a = IxedJSON Value
+
+ijMap :: (Value -> Value) -> IxedJSON a -> IxedJSON a
+ijMap f (IxedJSON a) = IxedJSON $ f a
+
+type instance Index   (IxedJSON a) = JSONPath ()
+type instance IxValue (IxedJSON a) = a
+
+others :: JSONPath a -> Value -> ([Value], [Value])
+others p v = let othersWith :: Ord i => IndexedTraversal' i Value Value -> i -> ([Value], [Value])
+                 othersWith l i = (those l (< i), those l (> i))
+                 those :: Ord i => IndexedTraversal' i Value Value -> (i -> Bool) -> [Value]
+                 those l f = fmap snd . sortBy (comparing fst) $ v ^@.. l . indices f in
+  case p of Here -> mempty
+            (AtKey k _ _) -> othersWith members k
+            (AtIx  i _ _) -> othersWith values i
+
+instance (FromJSON a, ToJSON a) => Ixed (IxedJSON a) where
+  ix Here f ij@(IxedJSON j) = case fromJSON j of
+    Error _   -> pure ij
+    Success v -> IxedJSON . toJSON <$> f v
+  ix i f ij@(IxedJSON j) = let inJ = ijMap $ \v -> j & tOf i .~ v in
+    maybe (pure ij) (fmap inJ) $ ix (nextPath i) f . IxedJSON <$> j ^? tOf i
+
+instance (FromJSON a, ToJSON a) => Authenticate (IxedJSON a) where
+  type HashFor  (IxedJSON a)  = (JSONPath () -> Bool, Digest SHA3_256)
+  type ProofFor (IxedJSON a) = JSONPath ([Digest SHA3_256], [Digest SHA3_256])
+  type Access   (IxedJSON a) = Maybe
+  retrieve Here j = (j ^? ix Here, Here)
+  retrieve p (IxedJSON j) = case retrieve (nextPath p) . IxedJSON <$> j ^? tOf p of
+    Just (Just v, p') -> (Just v, withStartOf p (join bimap (fmap jTopHash) $ others p j) p')
+    _                 -> (Nothing, Here)
+  digest (IxedJSON j) = (\p -> isJust $ j ^? tOf p, jTopHash j)
+  verify _ (f, _) Nothing  = not . f . fmap (const ())
+  verify _ (_, d) (Just v) = (d ==) . jFoldPath hashCons (jhash $ toJSON v)
--- test/Main.hs
+++ test/Main.hs
@@ -14,17 +14,26 @@ module Indurative.Test where
 import Control.Lens hiding (elements)
 import Control.Monad (ap)
 import Crypto.Hash (SHA3_256)
+import Data.Aeson (Value(..))
+import Data.Aeson.Lens (_Integral, key, members, nth, values)
+import Data.Bifunctor (Bifunctor(..))
 import Data.Binary (Binary)
 import Data.Proxy (Proxy(..))
+import Data.Text (pack)
 import Type.Reflection (Typeable, someTypeRep)
-import Indurative
 import Test.QuickCheck hiding (reason)
 import Test.QuickCheck.Property
+
+import qualified Data.HashMap.Strict as H (fromList)
+import qualified Data.Vector         as V (fromList)
 
 import Data.Array (Array, Ix)
 import Data.IntMap (IntMap)
 import Data.Map (Map)
 import Data.Sequence (Seq)
+
+import Indurative
+import Indurative.JSON
 
 deriving via Auth SHA3_256 (Map   k) v instance (Ord k, Binary k, Binary v) => Authenticate (Map   k v)
 deriving via Auth SHA3_256 IntMap    v instance Binary v =>                    Authenticate (IntMap  v)
@@ -54,12 +63,39 @@ faithful _ _ = let ty = Proxy :: Proxy (t x) in (show $ someTypeRep ty,) . prope
   let (res, p) = retrieve i t
   let got      = snd $ runAI res
   let real     = t ^? ix i
-  let mismatch = "asked for "++ show i ++" from "++ show t
-               ++" but got "++ show got ++" instead of "++ show real
-  let badProof = "asked for "++ show i ++" from "++ show t ++" but proof failed!"
+  let mismatch = "asked for " ++ show i   ++ " from " ++ show t
+              ++ " but got "  ++ show got ++ " instead of " ++ show real
+  let badProof = "asked for  "++ show i   ++ " from " ++ show t ++" but proof failed!"
   return $ if | got /= real -> failed {reason = mismatch}
               | not $ verify ty (digest t) res p -> failed {reason = badProof}
               | otherwise -> succeeded where
+
+testJson :: (String, Property)
+testJson = let genJSON 0 = (_Integral #) <$> (arbitrary :: Gen Int)
+               genJSON n = frequency [ (1, (_Integral #) <$> (arbitrary :: Gen Int))
+                                     , (5, Array  . V.fromList                    <$> listOf (genJSON $ n - 1))
+                                     , (5, Object . H.fromList . map (first pack) <$> ps n)
+                                     ]
+               ps n = zip <$> infiniteListOf arbitrary <*> listOf (genJSON $ n - 1)
+               genPath :: Value -> Gen (JSONPath ())
+               genPath a@(Array v)  = if null v then pure Here else
+                 elements (fst <$> a ^@.. values)  >>= \k -> AtIx  k () <$> genPath (a ^?! nth k)
+               genPath a@(Object v) = if null v then pure Here else
+                 elements (fst <$> a ^@.. members) >>= \k -> AtKey k () <$> genPath (a ^?! key k)
+               genPath _            = pure Here in
+  ("IxedJSON Int",) . property $ do
+    j <- genJSON (3 :: Integer)
+    p <- genPath j
+    let (res, r) = retrieve p (IxedJSON j)
+    let real     = IxedJSON j ^? ix p
+    let mismatch = "asked for " ++ show p   ++ " from " ++ show j
+                ++ " but got "  ++ show res ++ " instead of " ++ show real
+    let badProof = "asked for  "++ show p   ++ " from " ++ show j ++" but proof failed!"
+    return $ if | res /= real -> failed {reason = mismatch}
+                | not $ verify (Proxy :: Proxy (IxedJSON Int)) (digest (IxedJSON j :: IxedJSON Int))
+                               res r -> failed {reason = badProof}
+                | otherwise -> succeeded where
+
 
 onTs :: forall (t :: * -> *) r.
        (forall x. (Arbitrary x, Binary x, Eq x, Show x, Typeable x) => Proxy t -> Proxy x -> r)
@@ -73,7 +109,8 @@ onTs f p = [ f p (Proxy :: Proxy Int)
            ]
 
 props :: [(String, Property)]
-props = onTs faithful (Proxy :: Proxy IntMap)
+props = [testJson] 
+     ++ onTs faithful (Proxy :: Proxy IntMap)
      ++ onTs faithful (Proxy :: Proxy Maybe)
      ++ onTs faithful (Proxy :: Proxy [])
      ++ onTs faithful (Proxy :: Proxy Seq)
@@ -87,4 +124,4 @@ props = onTs faithful (Proxy :: Proxy IntMap)
 -}
 
 main :: IO ()
-main = mapM_ (\(n, p) -> putStrLn n >> quickCheckWith (stdArgs {maxShrinks = 10000}) p) props
+main = mapM_ (\(n, p) -> putStrLn n >> quickCheck p) props
